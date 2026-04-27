a87faf092409f54eb79f326491b46977e801cf83
Sambasiva Reddy Somu <samba.dev1234@gmail.com>
2026-04-27 15:07:26 -0700

    initial code

---
+++ src/main/java/com/home/kubernetes/spring_boot_k8s_sample1/SpringBootK8sSample1Application.java
@@ -0,0 +1,13 @@
+package com.home.kubernetes.spring_boot_k8s_sample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootK8sSample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootK8sSample1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/kubernetes/spring_boot_k8s_sample1/SpringBootK8sSample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.kubernetes.spring_boot_k8s_sample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootK8sSample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
